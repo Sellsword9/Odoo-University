e584b1bce97d8710505b4432b808357667109b00
sellsword9 <yerayromerodev@gmail.com>
2024-10-23 16:17:26 +0200

    Nitpciks y safeguard

--- models/dossier.py
+++ models/dossier.py
@@ -1,20 +1,17 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, tools
 
 class Dossier(models.Model):
     _name = 'university.dossier'
     _description = 'Dossier containing detailed data about students'
-
+    _auto = False
+    
     university = fields.Many2one('university.university')
     student = fields.Many2one('university.students')
     department = fields.Many2one('university.departments')
     professor = fields.Many2one('university.professors')
     subject = fields.Many2one('university.subjects')
     notes = fields.Many2many('university.notes')
-    average = fields.Float(compute='_compute_average', store=True)
-    
-    @api.depends('notes')
-    def _compute_average(self):
-        for record in self:
-            record.average = sum(record.notes.grade) / len(record.notes)
+    average = fields.Float()
+
             
     
