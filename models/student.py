e9a90d31516d53c6e812c444c16449bbff95ec98
sellsword9 <yerayromerodev@gmail.com>
2024-10-30 10:19:44 +0100

    Email wizard base

--- models/student.py
+++ models/student.py
@@ -55,11 +55,47 @@ class Student(models.Model):
                 record.average = sum / len(record.enrolls)
             else:
                 record.average = 0
-    
-    def action_send_pdf_email(self):
-        # Generate the PDF report
-        self.ensure_one()
-        template_id = self.env.ref('university.email_template_student').id
-        self.env['mail.template'].browse(template_id).send_mail(self.id, force_send=True)
         
+    def action_student_send(self):
+        """ Opens a wizard to compose an email, with relevant mail template loaded by default """
+
+        ctx = {
+            'default_model': 'university.students',
+            'default_res_ids': self.ids,
+            'default_composition_mode': 'comment',
+            'default_email_layout_xmlid': 'mail.mail_notification_layout_with_responsible_signature',
+        }
+
+        # Select mass mailing mode if multiple records are selected
+        if len(self) > 1:
+            ctx['default_composition_mode'] = 'mass_mail'
+        else:
+            # Update context for single emails
+            ctx.update({
+                'force_email': True,
+                'model_description': 'Student Report',
+            })
+            mail_template = self._find_mail_template()
+            if mail_template:
+                ctx.update({
+                    'default_template_id': mail_template.id,
+                    'mark_as_sent': True,
+                })
+
+        # Define action for email composition wizard
+        action = {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(False, 'form')],
+            'target': 'new',
+            'context': ctx,
+        }
+        return action
+
+    def _find_mail_template(self):
+        """ Get the appropriate mail template for the enrollment based on its state. """
+        self.ensure_one()
+        return self.env.ref('university.email_template_student', raise_if_not_found=False)
+
         
